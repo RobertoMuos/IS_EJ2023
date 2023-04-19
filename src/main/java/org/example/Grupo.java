484c824e909dda36646638cd3e3ecf86f43cd2f6
Werillo3 <werillo321@gmail.com>
2023-04-19 12:25:38 -0600

    Son los cambios que ha hecho Joel.. otra vez..

--- src/main/java/org/example/Grupo.java
+++ src/main/java/org/example/Grupo.java
@@ -1,7 +1,27 @@
 package org.example;
 
 public class Grupo {
+    private String RFC;
+    private String nombre;
 
+    public Grupo(String RFC, String nombre) {
+        this.RFC = RFC;
+        this.nombre = nombre;
+    }
 
+    public String getRFC() {
+        return RFC;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setRFC(String RFC) {
+        this.RFC = RFC;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
--- src/main/java/org/example/Materia.java
+++ src/main/java/org/example/Materia.java
@@ -1,7 +1,29 @@
 package org.example;
 
 public class Materia {
+    private int ID;
+    private String nombre;
 
+    public Materia(int ID, String nombre) {
+        this.ID = ID;
+        this.nombre = nombre;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 
 
 }
