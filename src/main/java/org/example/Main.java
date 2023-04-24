83a4a99ca6fa98e7ab7b005bb1430ab625abe3fa
RobertoMuos <roesmuos@gmail.com>
2023-04-24 13:28:19 -0600

    Se agrego un sout con mensjae
    BRANCH
    REMO_DEV

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Hola");
         System.out.println("Joel");
         System.out.println("Javier");
+        System.out.println("No chavos, noooo, los gringos no trabajan doble");
 
         Grupo grupos = new Grupo("2","A", new ArrayList<Estudiante>(){
 
